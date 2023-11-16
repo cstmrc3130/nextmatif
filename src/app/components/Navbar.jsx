9116ad8eb7ec2ee9d52fc20c815dd4440fd8a27d
CAESTO MARCO TAMPUBOLON <marcocaesto@gmail.com>
2023-11-16 16:09:52 +0700

    Navbar Fixation

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -23,6 +23,17 @@ const navItems = [
     },
 ];
 
+const ListItem = ({ children, navItemStyles, }) =>
+{
+    return (
+        <li>
+            <div className={`flex py-2 text-lg font-bold ${navItemStyles}`}>
+                {children}
+            </div>
+        </li>
+    );
+};
+
 export default function Navbar()
 {
     let pathname = usePathname() || "/";
@@ -34,52 +45,68 @@ export default function Navbar()
 
     const [hoveredPath, setHoveredPath] = useState(pathname);
     const [open, setOpen] = useState(false);
+    const navbarRef = useRef(null);
 
     return (
-        <div className="border border-stone-800/90 p-[0.4rem] rounded-lg mb-12 sticky top-0 z-[100] bg-stone-900/80 backdrop-blur-md">
-            <nav className="flex gap-2 relative justify-between w-full z-[1]  rounded-lg" >
-                <img src="https://himatifusu.org/img/logo/white/white_100.png" className="w-14 h-14" />
-                <div className={`${!open && "hidden"} sm:flex sm:flex-col`}>{navItems.map((item, index) =>
-                {
-                    const isActive = item.path === pathname;
+        <header className={`flex items-center justify-center w-full `}>
+            <div className="container">
+                <div className="relative flex items-center justify-between mx-4">
+                    <div className="max-w-full px-4">
+                        <a href="/#" className="block w-full py-5">
+                            <img src="https://himatifusu.org/img/logo/white/white_100.png" className="w-14 h-14" />
+                        </a>
+                    </div>
+                    <div ref={navbarRef} className="flex items-center justify-between w-full px-4" onClick={() => console.log("Hello")}>
+                        <nav id="navbarCollapse" className={`absolute top-[80%] left-0 right-0 w-full rounded-lg py-5 px-6 shadow z-10 bg-white lg:bg-transparent lg:basis-full lg:static lg:block lg:w-full lg:max-w-full lg:shadow-none ${!open && "hidden"} `}>
+                            <ul className="block lg:flex lg:items-center lg:justify-end gap-x-16 whitespace-nowrap">
+                                {navItems.map((item, index) =>
+                                {
+                                    const isActive = item.path === pathname;
 
-                    return (
-                        <Link
-                            key={item.path}
-                            className={`top-4 px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${isActive ? "text-zinc-100" : "text-zinc-400"
-                                }`}
-                            data-active={isActive}
-                            href={item.path}
-                            onMouseOver={() => setHoveredPath(item.path)}
-                            onMouseLeave={() => setHoveredPath(pathname)}
-                        >
-                            <span>{item.name}</span>
-                            {item.path === hoveredPath && (
-                                <motion.div
-                                    className="absolute bottom-0 left-0 h-full bg-stone-800/80 rounded-md -z-10"
-                                    layoutId="navbar"
-                                    aria-hidden="true"
-                                    style={{
-                                        width: "100%",
-                                    }}
-                                    transition={{
-                                        type: "spring",
-                                        bounce: 0.25,
-                                        stiffness: 130,
-                                        damping: 9,
-                                        duration: 0.3,
-                                    }}
-                                />
-                            )}
-                        </Link>
-                    );
-                })}</div>
-            </nav>
-            <button onClick={() => setOpen(!open)} id="navbarToggler" className={`${open && "navbarTogglerActive"} absolute right-4 top-1/2 block z-10  -translate-y-1/2 rounded-lg px-3 py-[6px] ring-indigo-300 focus:ring-2 lg:hidden`}>
-                <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "top-[7px] rotate-[45deg]"}`}></span>
-                <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "opacity-0"}`}></span>
-                <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "-top-2 -rotate-[45deg]"}`}></span>
-            </button>
-        </div>
+                                    return (
+                                        <ListItem navItemStyles="rounded-md lg:px-0" key={item.path} >
+                                            <Link className={`basis-full px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${isActive ? "text-zinc-700 lg:text-zinc-100" : "text-zinc-700 lg:text-zinc-300"}`} data-active={isActive} href={item.path} onMouseOver={() => setHoveredPath(item.path)} onMouseLeave={() => setHoveredPath(pathname)}>
+                                                <span>{item.name}</span>
+                                                {item.path === hoveredPath && (<motion.div className="absolute bottom-0 left-0 h-full w-full bg-sky-400/80 rounded-md -z-10" layoutId="navbar" aria-hidden="true" style={{ width: "100%", }} transition={{ type: "spring", bounce: 0.25, stiffness: 130, damping: 9, duration: 0.3, }} />)}
+                                            </Link>
+                                        </ListItem>
+                                    );
+                                })}
+                            </ul>
+                        </nav>
+                        <button onClick={() => setOpen(!open)} id="navbarToggler" className={`${open && "navbarTogglerActive"} absolute right-4 top-1/2 block z-10  -translate-y-1/2 rounded-lg px-3 py-[6px] ring-indigo-300 focus:ring-2 lg:hidden`}>
+                            <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "top-[7px] rotate-[45deg]"}`}></span>
+                            <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "opacity-0"}`}></span>
+                            <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "-top-2 -rotate-[45deg]"}`}></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
+
+
+// <div className="p-[0.4rem] rounded-lg mb-12 sticky top-0 z-[100] backdrop-blur-md">
+// <nav className="flex mx-auto container justify-between align-middle w-full z-[1] rounded-lg" >
+//     <img src="https://himatifusu.org/img/logo/white/white_100.png" className="w-14 h-14 self-center" />
+//     <div className={`w-full rounded-lg py-5 shadow z-10 bg-transparent block lg:flex lg:justify-end lg:w-full lg:max-w-full lg:shadow-none ${!open && "hidden"}`}>
+//         {navItems.map((item, index) =>
+//         {
+//             const isActive = item.path === pathname;
+
+//             return (
+//                 <Link key={item.path} className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${isActive ? "text-zinc-100" : "text-zinc-400"}`} data-active={isActive} href={item.path} onMouseOver={() => setHoveredPath(item.path)} onMouseLeave={() => setHoveredPath(pathname)}>
+//                     <span>{item.name}</span>
+//                     {item.path === hoveredPath && (<motion.div className="absolute bottom-0 left-0 h-full bg-stone-800/80 rounded-md -z-10" layoutId="navbar" aria-hidden="true" style={{ width: "100%", }} transition={{ type: "spring", bounce: 0.25, stiffness: 130, damping: 9, duration: 0.3, }} />)}
+//                 </Link>
+//             );
+//         })}
+//     </div>
+// </nav>
+// <button onClick={() => setOpen(!open)} id="navbarToggler" className={`${open && "navbarTogglerActive"} absolute right-4 top-1/2 block z-10  -translate-y-1/2 rounded-lg px-3 py-[6px] ring-indigo-300 focus:ring-2 lg:hidden`}>
+//     <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "top-[7px] rotate-[45deg]"}`}></span>
+//     <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "opacity-0"}`}></span>
+//     <span className={`relative my-[6px] block h-[2px] w-[30px] bg-white transition-all ${open && "-top-2 -rotate-[45deg]"}`}></span>
+// </button>
+// </div>
--- src/app/components/SkeletonSwiper.jsx
+++ src/app/components/SkeletonSwiper.jsx
@@ -4,15 +4,15 @@ const SkeletonSwiper = () =>
         <div className="p-2">
             <div className="flex items-center p-4 bg-white rounded hover:shadow-sky-200 hover:shadow-md hover:-translate-y-1 transition-all">
                 <div className="animate-pulse flex flex-shrink-0 items-center justify-center bg-transparent h-16 w-16 rounded">
-                    <div class="animate-pulse rounded-full bg-sky-700 h-10 w-10"></div>
+                    <div className="animate-pulse rounded-full bg-sky-700 h-10 w-10"></div>
                 </div>
                 <div className="flex-grow flex flex-col ml-4">
-                    <div class="animate-pulse space-y-3">
-                        <div class="grid grid-cols-3 gap-4">
-                            <div class="h-2 bg-sky-700 rounded col-span-2"></div>
-                            <div class="h-2 bg-sky-700 rounded col-span-1"></div>
+                    <div className="animate-pulse space-y-3">
+                        <div className="grid grid-cols-3 gap-4">
+                            <div className="h-2 bg-sky-700 rounded col-span-2"></div>
+                            <div className="h-2 bg-sky-700 rounded col-span-1"></div>
                         </div>
-                        <div class="h-2 bg-sky-700 rounded"></div>
+                        <div className="h-2 bg-sky-700 rounded"></div>
                     </div>
                 </div>
             </div>
